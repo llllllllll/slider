6c1544a1f7ec5a791a8931fc9b08878c01000138
de-odex <justinfblover@gmail.com>
2017-07-26 16:59:02 +0900

    Sanitize filenames in windows (#11)
    BUG: sanitize file name on windows

--- slider/library.py
+++ slider/library.py
@@ -4,6 +4,7 @@ from hashlib import md5
 import os
 import pathlib
 from time import sleep
+import sys
 
 import requests
 
@@ -47,6 +48,30 @@ class Cache:
     def keys(self):
         with self._dbm as d:
             return d.keys()
+
+
+if sys.platform.startswith('win'):
+    def sanitize_filename(name):
+        for invalid_character in r':*?"\/|<>[]':
+            name.replace(invalid_character, '')
+        return name
+else:
+    def sanitize_filename(name):
+        return name.replace('/', '')
+
+sanitize_filename.__doc__ = """\
+Sanitize a filename so that it is safe to write to the filesystem.
+
+Parameters
+----------
+name : str
+    The name of the file without the directory.
+
+Returns
+-------
+sanitized_name : str
+    The name with invalid characters stripped out.
+"""
 
 
 class Library:
@@ -259,13 +284,13 @@ class Library:
         if beatmap is None:
             beatmap = Beatmap.parse(data.decode('utf-8-sig'))
 
-        path = self.path / (
+        path = self.path / sanitize_filename(
             f'{beatmap.artist} - '
             f'{beatmap.title} '
             f'({beatmap.creator})'
             f'[{beatmap.version}]'
             f'.osu'
-        ).replace('/', '_')
+        )
         with open(path, 'wb') as f:
             f.write(data)
 
