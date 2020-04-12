47c487976479a24d9929f7e61f9e497cc4a40d45
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2020-04-12 14:38:47 +0200

    add basic tests for stacking

--- tests/test_beatmap.py
+++ tests/test_beatmap.py
@@ -4,6 +4,7 @@ import slider.curve
 from slider.position import Position
 from datetime import timedelta
 from math import isclose
+import numpy as np
 
 tatoe_path = "data/AKINO from bless4 & CHiCO with " + \
         "HoneyWorks - MIIRO vs. Ai no Scenario (monstrata) [Tatoe].osu"
@@ -72,8 +73,8 @@ def test_beatmap_parameters():
 
     # [Colours] (skipped)
     # [HitObjects]
-    # Only hit object 0 tested for now
-    hit_objects_0 = beatmap.hit_objects[0]
+    # test first hit_object
+    hit_objects_0 = beatmap.hit_objects(stacking=False)[0]
     assert hit_objects_0.position == Position(x=243, y=164)
     assert hit_objects_0.time == timedelta(milliseconds=1076)
     # Hit object note `type` is done by subclassing HitObject
@@ -93,6 +94,29 @@ def test_beatmap_parameters():
     assert hit_objects_0.edge_sounds == [2, 0]
     assert hit_objects_0.edge_additions == ['0:0', '0:0']
     assert hit_objects_0.addition == "0:0:0:0:"
+    # test stacked hit_objects
+    hit_objects_stacked = beatmap.hit_objects(stacking=True)
+    assert len(hit_objects_stacked) == len(beatmap.hit_objects(stacking=False))
+    assert np.array_equal(np.nonzero([h.stack_height for h in hit_objects_stacked]),
+                          [[7, 8, 10, 11, 16, 28, 29, 31, 35, 36, 38, 46, 50, 51,
+                            52, 54, 56, 58, 59, 60, 61, 62, 64, 67, 70, 71, 98,
+                            102, 106, 111, 114, 116, 122, 124, 125, 126, 128, 129,
+                            134, 135, 137, 138, 140, 141, 142, 144, 145, 147, 150,
+                            156, 162, 177, 180, 183, 191, 193, 194, 195, 196, 197,
+                            198, 199, 201, 203, 207, 208, 209, 210, 211, 213, 214,
+                            228, 240, 241, 242, 245, 246, 249, 251, 252, 253, 256,
+                            257, 258, 260, 261, 262, 263, 266, 283, 284, 288, 289,
+                            290, 291, 294, 295, 297, 301, 302, 303, 304, 305, 307,
+                            308, 309, 310, 313, 316, 320, 321, 322, 324, 325, 329]])
+    assert hit_objects_stacked[7].stack_height == 1
+    assert hit_objects_stacked[7].position == Position(x=281.352, y=223.352)
+    assert hit_objects_stacked[31].stack_height == -1
+    assert hit_objects_stacked[31].position == Position(x=286.648, y=230.648)
+    # test hard_rock hit_objects
+    hit_objects_hard_rock_0 = beatmap.hit_objects(hard_rock=True, stacking=False)[0]
+    assert hit_objects_hard_rock_0.position == Position(x=243, y=220)
+    assert hit_objects_0.curve.points == [Position(x=243, y=220),
+                                          Position(x=301, y=209)]
 
 
 def test_beatmap_methods():
