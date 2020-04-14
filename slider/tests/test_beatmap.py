46c40499d923fc0410bd12dd95fe3752dc35b710
Joe Jevnik <joe@quantopian.com>
2020-04-14 02:12:52 -0400

    TST: break up "mega tests" in `test_beatmap`
    "Mega tests" occur when a single test is testing multiple independent things. In
    general, a single test should test a single thing. This makes it much easier to
    diagnose a problem by looking at the set of test failures. Imagine making a
    change and seeing: `test failed: test_everything`. This doesn't tell the
    programmer _what_ is wrong, just that something is wrong. Instead, it might be
    better to see: `test failed: test_beatmap.test_ad`, this tells the programmer
    that something is likely wrong with the `Beatmap.ad` method. Maybe something is
    wrong with the beatmap parser getting the ad value, but at least it has been
    narrowed down to something being wrong with ad. The other test failures should
    help us narrow in on the actual root cause.
    
    I realize there aren't enough tests in slider to make this really work yet;
    however, it is better to be in the habit of writing small tests so that we don't
    need to break them up later.
    
    This PR breaks up the two tests in `test_beatmap.py` into one test per feature.
    I am currently leaving each beatmap section (Metadata, HitObjects, General,
    etc.) as a single test because it is unlikely that one thing in a section could
    be broken without all of them being broken.
    
    This PR also introduces a `pytest.fixture` to create the beatmap being examined.
    `pytest.fixture` allows one to reuse test set up so that each of these little
    tests doesn't need to start with the same beatmap parsing code.

--- slider/tests/test_beatmap.py
+++ slider/tests/test_beatmap.py
@@ -2,26 +2,36 @@ from datetime import timedelta
 from math import isclose
 import pathlib
 
+import pytest
+
 from slider import Beatmap
 import slider.beatmap
 import slider.curve
 from slider.position import Position
 
-tatoe_path = pathlib.Path(__file__).parent / (
-    'data/AKINO from bless4 & CHiCO with '
-    'HoneyWorks - MIIRO vs. Ai no Scenario (monstrata) [Tatoe].osu'
-)
+
+@pytest.fixture
+def beatmap():
+    return Beatmap.from_path(
+        pathlib.Path(__file__).parent / (
+            'data/AKINO from bless4 & CHiCO with '
+            'HoneyWorks - MIIRO vs. Ai no Scenario (monstrata) [Tatoe].osu'
+        ),
+    )
 
 
-def test_beatmap_parameters():
-    beatmap = Beatmap.from_path(tatoe_path)
+def test_version(beatmap):
     assert beatmap.format_version == 14
+
+
+def test_display_name(beatmap):
     assert beatmap.display_name == (
         'AKINO from bless4 & CHiCO with HoneyWorks - MIIRO '
         'vs. Ai no Scenario [Tatoe]'
     )
 
-    # [General]
+
+def test_parse_section_general(beatmap):
     assert beatmap.audio_filename == "tatoe.mp3"
     assert beatmap.audio_lead_in == timedelta()
     assert beatmap.preview_time == timedelta(milliseconds=6538)
@@ -32,13 +42,15 @@ def test_beatmap_parameters():
     assert not beatmap.letterbox_in_breaks
     assert not beatmap.widescreen_storyboard
 
-    # [Editor]
+
+def test_parse_section_editor(beatmap):
     assert beatmap.distance_spacing == 1.1
     assert beatmap.beat_divisor == 6
     assert beatmap.grid_size == 4
     assert beatmap.timeline_zoom == 1.8
 
-    # [Metadata]
+
+def test_parse_section_metadata(beatmap):
     assert beatmap.title == "MIIRO vs. Ai no Scenario"
     assert beatmap.title_unicode == "海色 vs. アイのシナリオ"
     assert beatmap.artist == "AKINO from bless4 & CHiCO with HoneyWorks"
@@ -79,7 +91,8 @@ def test_beatmap_parameters():
     assert beatmap.beatmap_id == 735272
     assert beatmap.beatmap_set_id == 325158
 
-    # [Difficulty]
+
+def test_parse_section_difficulty(beatmap):
     assert beatmap.hp_drain_rate == 6.5
     assert beatmap.circle_size == 4
     assert beatmap.overall_difficulty == 9
@@ -87,8 +100,9 @@ def test_beatmap_parameters():
     assert beatmap.slider_multiplier == 1.8
     assert beatmap.slider_tick_rate == 1
 
-    # [Events] (skipped)
-    # [TimingPoints] (the first one at least)
+
+def test_parse_section_timing_points(beatmap):
+    # currently only checking the first timing point
     timing_points_0 = beatmap.timing_points[0]
     assert timing_points_0.offset == timedelta()
     assert isclose(timing_points_0.ms_per_beat, 307.692307692308)
@@ -98,8 +112,8 @@ def test_beatmap_parameters():
     # inherited is not in class parameter
     assert timing_points_0.kiai_mode == 0
 
-    # [Colours] (skipped)
-    # [HitObjects]
+
+def test_parse_section_hit_objects(beatmap):
     # Only hit object 0 tested for now
     hit_objects_0 = beatmap.hit_objects[0]
     assert hit_objects_0.position == Position(x=243, y=164)
@@ -123,14 +137,25 @@ def test_beatmap_parameters():
     assert hit_objects_0.addition == "0:0:0:0:"
 
 
-def test_beatmap_methods():
-    # Difficulty calculations omitted
-    beatmap = Beatmap.from_path(tatoe_path)
-
-    # These functions take in mods
+def test_ar(beatmap):
     assert beatmap.ar() == 9.5
+
+
+def test_bpm_min(beatmap):
     assert beatmap.bpm_min() == 180
+
+
+def test_bpm_max(beatmap):
     assert beatmap.bpm_max() == 195
+
+
+def test_cs(beatmap):
     assert beatmap.cs() == 4
+
+
+def test_hp(beatmap):
     assert beatmap.hp() == 6.5  # issue #57
+
+
+def test_od(beatmap):
     assert beatmap.od() == 9
